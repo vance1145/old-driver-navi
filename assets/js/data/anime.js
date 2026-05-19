3d7c619303b2c878d317fa642ad33394f76ceab1
Van <vanjohnpk@gmail.com>
2026-05-20 02:53:32 +0800

    fix: 巴哈姆特动画疯添加图标 URL

--- assets/js/data/anime.js
+++ assets/js/data/anime.js
@@ -2,7 +2,7 @@ NAV_DATA.categories.push({
   id: "anime", name: "动漫", icon: "", links: [
     { title: "AGE 动漫", url: "https://www.age.tv/", desc: "在线动漫站｜高清新番/经典动画", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuYWdlLnR2Lw==" },
     { title: "Omofun", url: "https://www.mxfuns.com/", desc: "免费动漫在线看｜更新快速", icon: "" },
-    { title: "巴哈姆特动画疯", url: "https://ani.gamer.com.tw/", desc: "台湾正版动漫｜免费部分+付费解锁", icon: "" },
+    { title: "巴哈姆特动画疯", url: "https://ani.gamer.com.tw/", desc: "台湾正版动漫｜免费部分+付费解锁", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9hbmkuZ2FtZXIuY29tLnR3Lw==" },
     { title: "Bangumi", url: "https://bangumi.tv/", desc: "番组计划｜动画记录/评分/社交", icon: "" },
     { title: "萌番组", url: "https://bangumi.moe/", desc: "动漫 BT 种子下载｜追番索引", icon: "" },
     { title: "蜜柑计划", url: "https://mikanani.me/", desc: "RSS 追番工具｜自动订阅下载新番", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9taWthbmFuaS5tZS8=" },
