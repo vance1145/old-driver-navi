5f07f5127b28a90198f04769868d4f8abbc1e542
Van <vanjohnpk@gmail.com>
2026-05-20 03:33:44 +0800

    fix: Z-Library 链接改为 z-library.bz

--- assets/js/data/ebook.js
+++ assets/js/data/ebook.js
@@ -1,11 +1,8 @@
 NAV_DATA.categories.push({
   id: "ebook", name: "电子书", icon: "", links: [
-    { title: "GitHub", url: "https://github.com/", desc: "全球最大代码托管｜海量电子书/技术资源", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9naXRodWIuY29tLw==" },
-    { title: "Z-Library", url: "https://z-lib.io/", desc: "全球最大电子书库｜千万级资源", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly96LWxpYi5pby8=" },
-    { title: "Library Genesis", url: "https://libgen.is/", desc: "学术/电子书｜论文/教材/小说", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9saWJnZW4uaXMv" },
+    { title: "GitHub", url: "https://github.com/", desc: "全球最大代码托管｜上面的电子书真的很多", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9naXRodWIuY29tLw==" },
+    { title: "Z-Library", url: "https://z-library.bz/", desc: "全球最大电子书库｜千万级资源", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly96LWxpYnJhcnkuYnov" },
     { title: "Project Gutenberg", url: "https://www.gutenberg.org/", desc: "全球最大免费电子书库｜6 万+ 经典", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuZ3V0ZW5iZXJnLm9yZy8=" },
-    { title: "安娜的档案", url: "https://zh.annas-archive.li/", desc: "影子图书馆搜索引擎｜聚合多源", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly96aC5hbm5hcy1hcmNoaXZlLmxpLw==" },
-    { title: "鸠摩搜书", url: "https://www.jiumodiary.com/", desc: "电子书搜索引擎｜聚合多源", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuaml1bW9kaWFyeS5jb20v" },
     { title: "微信读书", url: "https://weread.qq.com/", desc: "微信读书｜社交化阅读体验", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93ZXJlYWQucXEuY29tLw==" },
     { title: "SoBooks", url: "https://sobooks.cc/", desc: "免费电子书｜资源多/格式全", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9zb2Jvb2tzLmNjLw==" },
     { title: "苦瓜书盘", url: "https://kgbook.com/", desc: "免费电子书｜免登录直接下载", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9rZ2Jvb2suY29tLw==" },
