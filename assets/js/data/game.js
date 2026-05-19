963aaca54bc08f38f635cf8481c543bad1ac04dc
Van <vanjohnpk@gmail.com>
2026-05-20 02:24:11 +0800

    feat: 新增「游戏」18 个站点、「动漫」18 个站点
    游戏：Steam / Epic / DLSite / Switch520 / GOG / itch.io / XboxGP
          PSN / Ubisoft / 游民星空 / 3DM / SteamDB / SteamCharts
          HowLongToBeat / Metacritic / 野炊社 / 游侠网 / 大游戏
    
    动漫：B站 / AGE动漫 / Omofun / 樱花动漫 / AcFun / 动漫花园
          蜜柑计划 / Bangumi / 萌番组 / 动漫之家 / 拷贝漫画
          MyAnimeList / AniList / Crunchyroll / AnimeDB / 风车动漫 / 嘀哩嘀哩 / 动漫星空

---
+++ assets/js/data/game.js
@@ -0,0 +1,22 @@
+NAV_DATA.categories.push({
+  id: "game", name: "游戏", icon: "", links: [
+    { title: "Steam", url: "https://store.steampowered.com/", desc: "全球最大 PC 游戏平台｜夏促/秋促/冬促", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9zdG9yZS5zdGVhbXBvd2VyZWQuY29tLw==" },
+    { title: "Epic Games", url: "https://store.epicgames.com/", desc: "Epic 游戏商店｜每周免费领游戏", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9zdG9yZS5lcGljZ2FtZXMuY29tLw==" },
+    { title: "DLSite", url: "https://www.dlsite.com/", desc: "日本同人游戏/音声/漫画平台", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuZGxzaXRlLmNvbS8=" },
+    { title: "Switch520", url: "https://www.switch520.com/", desc: "Nintendo Switch 游戏下载｜汉化资源", icon: "" },
+    { title: "GOG", url: "https://www.gog.com/", desc: "CDPR 旗下｜无 DRM 老游戏/独立游戏", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuZ29nLmNvbS8=" },
+    { title: "itch.io", url: "https://itch.io/", desc: "独立游戏发布平台｜海量免费试玩", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9pdGNoLmlvLw==" },
+    { title: "Xbox Game Pass", url: "https://www.xbox.com/xbox-game-pass", desc: "微软游戏订阅｜PC 百款游戏畅玩", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cueGJveC5jb20veGJveC1nYW1lLXBhc3M=" },
+    { title: "PlayStation Store", url: "https://store.playstation.com/", desc: "PS4/PS5 官方商店｜数字版/折扣", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9zdG9yZS5wbGF5c3RhdGlvbi5jb20v" },
+    { title: "Ubisoft Store", url: "https://store.ubi.com/", desc: "育碧游戏商城｜刺客信条/孤岛惊魂", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9zdG9yZS51YmkuY29tLw==" },
+    { title: "游民星空", url: "https://www.gamersky.com/", desc: "国内最大单机游戏媒体｜攻略/评测/下载", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuZ2FtZXJza3kuY29tLw==" },
+    { title: "3DM", url: "https://www.3dmgame.com/", desc: "单机游戏资源站｜汉化补丁/修改器", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuM2RtZ2FtZS5jb20v" },
+    { title: "SteamDB", url: "https://steamdb.info/", desc: "Steam 数据查询｜史低价格/折扣历史", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9zdGVhbWRiLmluZm8v" },
+    { title: "Steam Charts", url: "https://steamcharts.com/", desc: "Steam 在线人数统计｜热门游戏排行", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9zdGVhbWNoYXJ0cy5jb20v" },
+    { title: "HowLongToBeat", url: "https://howlongtobeat.com/", desc: "游戏通关时长查询｜买前必看", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9ob3dsb25ndG9iZWF0LmNvbS8=" },
+    { title: "Metacritic", url: "https://www.metacritic.com/", desc: "媒体/用户评分聚合｜买游戏参考", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubWV0YWNyaXRpYy5jb20v" },
+    { title: "野炊社", url: "https://www.yechuishe.com/", desc: "Switch 游戏攻略｜游戏资源分享", icon: "" },
+    { title: "游侠网", url: "https://www.ali213.net/", desc: "老牌单机游戏站｜补丁/修改器/攻略", icon: "" },
+    { title: "大游戏", url: "https://www.dyx.com/", desc: "中文游戏下载站｜单机/汉化/整合版", icon: "" },
+  ]
+});
