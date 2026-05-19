d193cf7573f705a2e9a00c38eac4c2dde8970ae9
Van <vanjohnpk@gmail.com>
2026-05-20 03:16:07 +0800

    feat: 漫画新增 KZO / Komiic / 悠悠漫画

--- assets/js/data/manga.js
+++ assets/js/data/manga.js
@@ -2,12 +2,14 @@ NAV_DATA.categories.push({
   id: "manga", name: "漫画", icon: "", links: [
     { title: "MangaDex", url: "https://mangadex.org/", desc: "全球最大免费漫画站｜多语言/翻译组", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9tYW5nYWRleC5vcmcv" },
     { title: "MangaPlus", url: "https://mangaplus.shueisha.co.jp/", desc: "集英社官方免费｜海贼/咒术/间谍过家家", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9tYW5nYXBsdXMuc2h1ZWlzaGEuY28uanAv" },
-    { title: "动漫之家", url: "https://www.dmzj.com/", desc: "国内最大在线漫画｜日漫/国漫/轻小说", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuZG16ai5jb20v" },
     { title: "拷贝漫画", url: "https://www.mangacopy.com/", desc: "在线漫画阅读｜更新快/画质高", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubWFuZ2Fjb3B5LmNvbS8=" },
     { title: "哔哩哔哩漫画", url: "https://manga.bilibili.com/", desc: "B 站漫画｜正版日漫/国漫独家", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9tYW5nYS5iaWxpYmlsaS5jb20v" },
     { title: "腾讯动漫", url: "https://ac.qq.com/", desc: "腾讯动漫｜国漫原创平台", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9hYy5xcS5jb20v" },
     { title: "快看漫画", url: "https://www.kuaikanmanhua.com/", desc: "国漫社区｜条漫/原创/社交", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cua3VhaWthbm1hbmh1YS5jb20v" },
     { title: "漫客栈", url: "https://www.mkzhan.com/", desc: "国漫在线看｜玄幻/恋爱/搞笑", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubWt6aGFuLmNvbS8=" },
     { title: "ComicWalker", url: "https://comic-walker.com/", desc: "角川官方免费｜KADOKAWA 作品", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9jb21pYy13YWxrZXIuY29tLw==" },
+    { title: "KZO", url: "https://kzo.moe/", desc: "在线漫画阅读｜免费高清", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9rem8ubW9lLw==" },
+    { title: "Komiic", url: "https://komiic.com/", desc: "漫画在线阅读｜更新及时", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9rb21paWMuY29tLw==" },
+    { title: "悠悠漫画", url: "https://www.yymanhua.com/", desc: "国漫/日漫在线看｜免费", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cueXltYW5odWEuY29tLw==" },
   ]
 });
