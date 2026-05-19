935d2ad9a3128557203333ebfcd983de2ff9e22e
Van <vanjohnpk@gmail.com>
2026-05-20 02:56:29 +0800

    feat: 新增「影视」分类 10 个站点

---
+++ assets/js/data/movie.js
@@ -0,0 +1,14 @@
+NAV_DATA.categories.push({
+  id: "movie", name: "影视", icon: "", links: [
+    { title: "Netflix", url: "https://www.netflix.com/", desc: "全球最大流媒体｜原创剧集/电影/纪录片", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubmV0ZmxpeC5jb20v" },
+    { title: "Disney+", url: "https://www.disneyplus.com/", desc: "迪士尼/漫威/星球大战/国家地理", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuZGlzbmV5cGx1cy5jb20v" },
+    { title: "HBO", url: "https://www.hbo.com/", desc: "HBO 精品剧集｜权游/最后生还者/继承之战", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuaGJvLmNvbS8=" },
+    { title: "Amazon Prime Video", url: "https://www.primevideo.com/", desc: "亚马逊流媒体｜高堡奇人/黑袍纠察队", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cucHJpbWV2aWRlby5jb20v" },
+    { title: "Apple TV+", url: "https://tv.apple.com/", desc: "苹果原创｜基地/人生切割术/足球教练", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly90di5hcHBsZS5jb20v" },
+    { title: "人人影视", url: "https://yyets.click/", desc: "字幕组经典｜美剧/英剧/日剧下载", icon: "" },
+    { title: "茶杯狐", url: "https://cupfox.app/", desc: "影视搜索引擎｜找片神器", icon: "" },
+    { title: "NO视频", url: "https://www.novipnoad.us/", desc: "免费高清影视｜无需注册即看", icon: "" },
+    { title: "字幕库", url: "https://zimuku.org/", desc: "外挂字幕下载｜电影/剧集字幕", icon: "" },
+    { title: "SubHD", url: "https://subhd.tv/", desc: "字幕分享社区｜中英字幕", icon: "" },
+  ]
+});
