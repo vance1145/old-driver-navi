931f344c4f9c9c16dd41964d958eabde04acb2d1
Van <vanjohnpk@gmail.com>
2026-05-20 03:05:15 +0800

    feat: 新增漫画(10)/轻小说(10)/电子书(10) 三个分类

---
+++ assets/js/data/manga.js
@@ -0,0 +1,14 @@
+NAV_DATA.categories.push({
+  id: "manga", name: "漫画", icon: "", links: [
+    { title: "MangaDex", url: "https://mangadex.org/", desc: "全球最大免费漫画站｜多语言/翻译组", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9tYW5nYWRleC5vcmcv" },
+    { title: "MangaPlus", url: "https://mangaplus.shueisha.co.jp/", desc: "集英社官方免费｜海贼/咒术/间谍过家家", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9tYW5nYXBsdXMuc2h1ZWlzaGEuY28uanAv" },
+    { title: "动漫之家", url: "https://www.dmzj.com/", desc: "国内最大在线漫画｜日漫/国漫/轻小说", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuZG16ai5jb20v" },
+    { title: "拷贝漫画", url: "https://www.mangacopy.com/", desc: "在线漫画阅读｜更新快/画质高", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubWFuZ2Fjb3B5LmNvbS8=" },
+    { title: "哔哩哔哩漫画", url: "https://manga.bilibili.com/", desc: "B 站漫画｜正版日漫/国漫独家", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9tYW5nYS5iaWxpYmlsaS5jb20v" },
+    { title: "腾讯动漫", url: "https://ac.qq.com/", desc: "腾讯动漫｜国漫原创平台", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9hYy5xcS5jb20v" },
+    { title: "快看漫画", url: "https://www.kuaikanmanhua.com/", desc: "国漫社区｜条漫/原创/社交", icon: "" },
+    { title: "漫客栈", url: "https://www.mkzhan.com/", desc: "国漫在线看｜玄幻/恋爱/搞笑", icon: "" },
+    { title: "有妖气", url: "https://www.u17.com/", desc: "原创漫画平台｜十万个冷笑话", icon: "" },
+    { title: "ComicWalker", url: "https://comic-walker.com/", desc: "角川官方免费｜KADOKAWA 作品", icon: "" },
+  ]
+});
---
+++ assets/js/data/novel.js
@@ -0,0 +1,14 @@
+NAV_DATA.categories.push({
+  id: "novel", name: "轻小说", icon: "", links: [
+    { title: "轻之国度", url: "https://www.lightnovel.us/", desc: "轻小说论坛｜日轻翻译/交流社区", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubGlnaHRub3ZlbC51cy8=" },
+    { title: "轻小说文库", url: "https://www.wenku8.net/", desc: "免费轻小说阅读｜日轻/国轻/文库", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cud2Vua3U4Lm5ldC8=" },
+    { title: "哔哩轻小说", url: "https://www.bilinovel.com/", desc: "B 站轻小说｜正版日轻/国轻", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuYmlsaW5vdmVsLmNvbS8=" },
+    { title: "真白萌", url: "https://masiro.me/", desc: "轻小说翻译分享｜Web 文库版", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9tYXNpcm8ubWUv" },
+    { title: "成为小说家吧", url: "https://ncode.syosetu.com/", desc: "日本最大网络小说站｜转生/异世界", icon: "" },
+    { title: "NovelUpdates", url: "https://www.novelupdates.com/", desc: "英文轻小说追踪｜连载更新查询", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubm92ZWx1cGRhdGVzLmNvbS8=" },
+    { title: "BookWalker", url: "https://global.bookwalker.jp/", desc: "角川官方电子书｜日轻正版购买", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9nbG9iYWwuYm9va3dhbGtlci5qcC8=" },
+    { title: "Kakuyomu", url: "https://kakuyomu.jp/", desc: "角川网络小说平台｜日本 Web 小说", icon: "" },
+    { title: "Sinoalice", url: "https://www.sinoalice.com/", desc: "轻小说/游戏衍生作品", icon: "" },
+    { title: "爱丽丝轻小说", url: "https://www.alcsnovel.com/", desc: "轻小说在线阅读｜日轻翻译", icon: "" },
+  ]
+});
