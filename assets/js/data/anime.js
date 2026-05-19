b8eff2af5f5a55ac5f9624f7e595948d4f4c355d
Van <vanjohnpk@gmail.com>
2026-05-20 02:42:08 +0800

    feat: 动漫扩展至 18 个站点 — 新增 ZzzFun/动漫岛/动画疯/AniWatch/9anime/Gogoanime/AnimePahe/Zoro/萌番组/天使动漫/嘀哩嘀哩

--- assets/js/data/anime.js
+++ assets/js/data/anime.js
@@ -2,7 +2,21 @@ NAV_DATA.categories.push({
   id: "anime", name: "动漫", icon: "", links: [
     { title: "AGE 动漫", url: "https://www.age.tv/", desc: "在线动漫站｜高清新番/经典动画", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuYWdlLnR2Lw==" },
     { title: "Omofun", url: "https://www.mxfuns.com/", desc: "免费动漫在线看｜更新快速", icon: "" },
+    { title: "樱花动漫", url: "https://www.yhdm.tv/", desc: "老牌在线动漫站｜番剧齐全", icon: "" },
+    { title: "风车动漫", url: "https://www.fengche.tv/", desc: "在线动漫观看｜高清画质", icon: "" },
+    { title: "ZzzFun", url: "https://www.zzzfun.com/", desc: "免费动漫在线｜无广告体验好", icon: "" },
+    { title: "动漫岛", url: "https://www.dmdm.tv/", desc: "动漫在线观看｜日漫/国漫", icon: "" },
+    { title: "巴哈姆特动画疯", url: "https://ani.gamer.com.tw/", desc: "台湾正版动漫｜免费部分+付费解锁", icon: "" },
+    { title: "AniWatch", url: "https://aniwatch.to/", desc: "英文｜免费动漫在线｜界面现代", icon: "" },
+    { title: "9anime", url: "https://9anime.to/", desc: "英文｜全球最大免费动漫站之一", icon: "" },
+    { title: "Gogoanime", url: "https://gogoanime.sx/", desc: "英文｜老牌免费动漫｜更新及时", icon: "" },
+    { title: "AnimePahe", url: "https://animepahe.ru/", desc: "英文｜高清动漫下载/在线", icon: "" },
+    { title: "Zoro.to", url: "https://zoro.to/", desc: "英文｜无广告动漫站｜体验极佳", icon: "" },
     { title: "Bangumi", url: "https://bangumi.tv/", desc: "番组计划｜动画记录/评分/社交", icon: "" },
-
+    { title: "萌番组", url: "https://bangumi.moe/", desc: "动漫 BT 种子下载｜追番索引", icon: "" },
+    { title: "蜜柑计划", url: "https://mikanani.me/", desc: "RSS 追番工具｜自动订阅下载新番", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9taWthbmFuaS5tZS8=" },
+    { title: "动漫花园", url: "https://share.dmhy.org/", desc: "动漫资源 BT 分享｜追番下载必备", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9zaGFyZS5kbWh5Lm9yZy8=" },
+    { title: "天使动漫", url: "https://www.tsdm39.com/", desc: "动漫论坛｜资源分享/音乐/美图", icon: "" },
+    { title: "嘀哩嘀哩", url: "https://www.dilidili.wang/", desc: "二次元追番站｜新番连载", icon: "" },
   ]
 });
--- assets/js/data/game.js
+++ assets/js/data/game.js
@@ -5,8 +5,8 @@ NAV_DATA.categories.push({
     { title: "Epic Games", url: "https://store.epicgames.com/", desc: "Epic 游戏商店｜每周免费领游戏", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9zdG9yZS5lcGljZ2FtZXMuY29tLw==" },
     { title: "DLSite", url: "https://www.dlsite.com/", desc: "日本同人游戏/音声/漫画平台", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuZGxzaXRlLmNvbS8=" },
     { title: "itch.io", url: "https://itch.io/", desc: "独立游戏发布平台｜海量免费试玩", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9pdGNoLmlvLw==" },
-    { title: "Switch520", url: "https://www.switch520.com/", desc: "用过都说好，谢谢老区长", icon: "" },
     { title: "Nexus Mods", url: "https://www.nexusmods.com/", desc: "全球最大游戏模组社区｜老滚/辐射/星空", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubmV4dXNtb2RzLmNvbS8=" },
     { title: "Poki", url: "https://poki.com/zh", desc: "在线小游戏平台｜上千款免费即玩", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9wb2tpLmNvbS8=" },
+    { title: "Switch520", url: "https://www.switch520.com/", desc: "用过都说好，谢谢老区长", icon: "" },
   ]
 });
