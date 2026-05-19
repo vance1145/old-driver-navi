5b9360d9a8f46dd0b501b7736423d9c7710e57f5
Van <vanjohnpk@gmail.com>
2026-05-20 02:55:19 +0800

    chore: 鲲Gal / GGBase 尝试添加 favicon

--- assets/js/data/anime.js
+++ assets/js/data/anime.js
@@ -1,12 +1,12 @@
 NAV_DATA.categories.push({
   id: "anime", name: "动漫", icon: "", links: [
     { title: "AGE 动漫", url: "https://www.age.tv/", desc: "在线动漫站｜高清新番/经典动画", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuYWdlLnR2Lw==" },
-    { title: "Omofun", url: "https://www.mxfuns.com/", desc: "免费动漫在线看｜更新快速", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubXhmdW5zLmNvbS8=" },
+    { title: "Omofun", url: "https://www.mxfuns.com/", desc: "免费动漫在线看｜更新快速", icon: "" },
     { title: "巴哈姆特动画疯", url: "https://ani.gamer.com.tw/", desc: "台湾正版动漫｜免费部分+付费解锁", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9hbmkuZ2FtZXIuY29tLnR3Lw==" },
-    { title: "Bangumi", url: "https://bangumi.tv/", desc: "番组计划｜动画记录/评分/社交", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9iYW5ndW1pLnR2Lw==" },
-    { title: "萌番组", url: "https://bangumi.moe/", desc: "动漫 BT 种子下载｜追番索引", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9iYW5ndW1pLm1vZS8=" },
+    { title: "Bangumi", url: "https://bangumi.tv/", desc: "番组计划｜动画记录/评分/社交", icon: "" },
+    { title: "萌番组", url: "https://bangumi.moe/", desc: "动漫 BT 种子下载｜追番索引", icon: "" },
     { title: "蜜柑计划", url: "https://mikanani.me/", desc: "RSS 追番工具｜自动订阅下载新番", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9taWthbmFuaS5tZS8=" },
-    { title: "动漫花园", url: "https://share.dmhy.org/", desc: "动漫资源 BT 分享｜追番下载必备", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9zaGFyZS5kbWh5Lm9yZy8=" },
+    { title: "动漫花园", url: "https://share.dmhy.org/", desc: "动漫资源 BT 分享｜追番下载必备", icon: "" },
     { title: "MyAnimeList", url: "https://myanimelist.net/", desc: "全球最大动漫数据库｜评分/排行/追番记录", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9teWFuaW1lbGlzdC5uZXQv" },
     { title: "AcFun", url: "https://www.acfun.cn/", desc: "弹幕网鼻祖｜番剧/动画/影视", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuYWNmdW4uY24v" },
     { title: "哔哩哔哩", url: "https://www.bilibili.com/", desc: "国内最大弹幕视频网｜番剧/动画/二次元", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuYmlsaWJpbGkuY29tLw==" },
--- assets/js/data/game.js
+++ assets/js/data/game.js
@@ -7,8 +7,8 @@ NAV_DATA.categories.push({
     { title: "Nexus Mods", url: "https://www.nexusmods.com/", desc: "全球最大游戏模组社区｜老滚/辐射/星空", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubmV4dXNtb2RzLmNvbS8=" },
     { title: "SteamDB", url: "https://steamdb.info/", desc: "Steam 数据查询｜史低价格/折扣历史", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9zdGVhbWRiLmluZm8v" },
     { title: "Switch520", url: "https://www.switch520.com/", desc: "用过都说好，谢谢老区长", icon: "" },
-    { title: "鲲Gal", url: "https://www.kungal.com/", desc: "GalGame 交流社区｜游戏资源/汉化", icon: "" },
-    { title: "GGBase", url: "https://ggbases.dlgal.com/", desc: "GalGame 资源站｜汉化/硬盘版下载", icon: "" },
+    { title: "鲲Gal", url: "https://www.kungal.com/", desc: "GalGame 交流社区｜游戏资源/汉化", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cua3VuZ2FsLmNvbS8=" },
+    { title: "GGBase", url: "https://ggbases.dlgal.com/", desc: "GalGame 资源站｜汉化/硬盘版下载", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9nZ2Jhc2VzLmRsZ2FsLmNvbS8=" },
     { title: "Poki", url: "https://poki.com/zh", desc: "在线小游戏平台｜上千款免费即玩", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9wb2tpLmNvbS8=" },
   ]
 });
