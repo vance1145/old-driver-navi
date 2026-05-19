374633d2cb6c438265a3421eb02c3663f2d5853d
Van <vanjohnpk@gmail.com>
2026-05-20 02:40:24 +0800

    feat: 游戏新增 Nexus Mods / Poki，Omofun/Bangumi 改首字符占位

--- assets/js/data/anime.js
+++ assets/js/data/anime.js
@@ -1,8 +1,8 @@
 NAV_DATA.categories.push({
   id: "anime", name: "动漫", icon: "", links: [
     { title: "AGE 动漫", url: "https://www.age.tv/", desc: "在线动漫站｜高清新番/经典动画", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuYWdlLnR2Lw==" },
-    { title: "Omofun", url: "https://www.mxfuns.com/", desc: "免费动漫在线看｜更新快速", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubXhmdW5zLmNvbS8=" },
-    { title: "Bangumi", url: "https://bangumi.tv/", desc: "番组计划｜动画记录/评分/社交", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9iYW5ndW1pLnR2Lw==" },
+    { title: "Omofun", url: "https://www.mxfuns.com/", desc: "免费动漫在线看｜更新快速", icon: "" },
+    { title: "Bangumi", url: "https://bangumi.tv/", desc: "番组计划｜动画记录/评分/社交", icon: "" },
 
   ]
 });
--- assets/js/data/game.js
+++ assets/js/data/game.js
@@ -6,5 +6,7 @@ NAV_DATA.categories.push({
     { title: "DLSite", url: "https://www.dlsite.com/", desc: "日本同人游戏/音声/漫画平台", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuZGxzaXRlLmNvbS8=" },
     { title: "itch.io", url: "https://itch.io/", desc: "独立游戏发布平台｜海量免费试玩", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9pdGNoLmlvLw==" },
     { title: "Switch520", url: "https://www.switch520.com/", desc: "用过都说好，谢谢老区长", icon: "" },
+    { title: "Nexus Mods", url: "https://www.nexusmods.com/", desc: "全球最大游戏模组社区｜老滚/辐射/星空", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubmV4dXNtb2RzLmNvbS8=" },
+    { title: "Poki", url: "https://poki.com/zh", desc: "在线小游戏平台｜上千款免费即玩", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9wb2tpLmNvbS8=" },
   ]
 });
