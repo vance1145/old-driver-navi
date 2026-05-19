12b967715bc5605a542363f9b474a5427bd0e095
Van <vanjohnpk@gmail.com>
2026-05-20 02:09:42 +0800

    feat: 🔞 新增 hanime1 / jable.tv

--- assets/js/data/plus.js
+++ assets/js/data/plus.js
@@ -17,5 +17,7 @@ NAV_DATA.categories.push({
     { title: "Pornhub", url: "https://www.pornhub.com/", desc: "全球最大成人平台｜海量免费内容", icon: "" },
     { title: "XVideos", url: "https://www.xvideos.com/", desc: "流量最高的成人视频站｜更新极快", icon: "" },
     { title: "xHamster", url: "https://xhamster.com/", desc: "用户量大｜免费+付费混合模式", icon: "" },
+    { title: "hanime1", url: "https://hanime1.me/", desc: "动漫成人内容｜二次元向", icon: "" },
+    { title: "jable.tv", url: "https://jable.tv/", desc: "日本成人视频｜高清资源", icon: "" },
   ]
 });
