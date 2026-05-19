f9cfcf056618c289c9498f2593025c0337f8d46b
Van <vanjohnpk@gmail.com>
2026-05-20 03:18:06 +0800

    feat: 漫画新增 X漫画/MangaBZ/MyComic/漫画柜/搜漫

--- assets/js/data/manga.js
+++ assets/js/data/manga.js
@@ -11,5 +11,10 @@ NAV_DATA.categories.push({
     { title: "KZO", url: "https://kzo.moe/", desc: "在线漫画阅读｜免费高清", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9rem8ubW9lLw==" },
     { title: "Komiic", url: "https://komiic.com/", desc: "漫画在线阅读｜更新及时", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9rb21paWMuY29tLw==" },
     { title: "悠悠漫画", url: "https://www.yymanhua.com/", desc: "国漫/日漫在线看｜免费", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cueXltYW5odWEuY29tLw==" },
+    { title: "X漫画", url: "https://www.xmanhua.com/", desc: "在线漫画阅读｜日漫/国漫", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cueG1hbmh1YS5jb20v" },
+    { title: "MangaBZ", url: "https://mangabz.com/", desc: "英文漫画站｜日漫翻译", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9tYW5nYWJ6LmNvbS8=" },
+    { title: "MyComic", url: "https://mycomic.com/cn", desc: "漫画在线阅读｜日漫/国漫", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9teWNvbWljLmNvbS9jbg==" },
+    { title: "漫画柜", url: "https://www.manhuagui.com/", desc: "漫画在线阅读｜老牌漫画站", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubWFuaHVhZ3VpLmNvbS8=" },
+    { title: "搜漫", url: "https://www.soman.com/", desc: "漫画搜索引擎｜找漫神器", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuc29tYW4uY29tLw==" },
   ]
 });
