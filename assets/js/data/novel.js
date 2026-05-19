70c393146ebd98c4fe23704d0b8c6dbcee5b5a2b
Van <vanjohnpk@gmail.com>
2026-05-20 03:26:34 +0800

    fix: 移除已关闭的轻小说文库，真白萌/魔笔小说降级首字符

--- assets/js/data/novel.js
+++ assets/js/data/novel.js
@@ -1,14 +1,13 @@
 NAV_DATA.categories.push({
   id: "novel", name: "轻小说", icon: "", links: [
-    { title: "轻之国度", url: "https://www.lightnovel.us/", desc: "轻小说论坛｜日轻翻译/交流社区", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubGlnaHRub3ZlbC51cy8=" },
-    { title: "轻小说文库", url: "https://www.wenku8.net/", desc: "免费轻小说阅读｜日轻/国轻/文库", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cud2Vua3U4Lm5ldC8=" },
-    { title: "哔哩轻小说", url: "https://www.bilinovel.com/", desc: "B 站轻小说｜正版日轻/国轻", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuYmlsaW5vdmVsLmNvbS8=" },
-    { title: "真白萌", url: "https://masiro.me/", desc: "轻小说翻译分享｜Web 文库版", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9tYXNpcm8ubWUv" },
-    { title: "成为小说家吧", url: "https://ncode.syosetu.com/", desc: "日本最大网络小说站｜转生/异世界", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9uY29kZS5zeW9zZXR1LmNvbS8=" },
+    { title: "轻之国度", url: "https://www.lightnovel.fun/", desc: "国内最大轻小说论坛｜日轻翻译/交流社区", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubGlnaHRub3ZlbC5mdW4v" },
+    { title: "哔哩轻小说", url: "https://www.linovelib.com/", desc: "在线阅读/TXT下载｜日轻国轻资源", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubGlub3ZlbGliLmNvbS8=" },
+    { title: "真白萌", url: "https://masiro.me/", desc: "轻小说翻译分享｜Web 文库版", icon: "" },
+    { title: "魔笔小说", url: "https://www.mobinovels.com/", desc: "EPUB/MOBI 下载｜轻小说电子书", icon: "" },
+    { title: "成为小说家吧", url: "https://ncode.syosetu.com/", desc: "日本最大 Web 小说站｜转生/异世界", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9uY29kZS5zeW9zZXR1LmNvbS8=" },
     { title: "NovelUpdates", url: "https://www.novelupdates.com/", desc: "英文轻小说追踪｜连载更新查询", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubm92ZWx1cGRhdGVzLmNvbS8=" },
     { title: "BookWalker", url: "https://global.bookwalker.jp/", desc: "角川官方电子书｜日轻正版购买", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9nbG9iYWwuYm9va3dhbGtlci5qcC8=" },
     { title: "Kakuyomu", url: "https://kakuyomu.jp/", desc: "角川网络小说平台｜日本 Web 小说", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9rYWt1eW9tdS5qcC8=" },
-    { title: "Sinoalice", url: "https://www.sinoalice.com/", desc: "轻小说/游戏衍生作品", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuc2lub2FsaWNlLmNvbS8=" },
-    { title: "爱丽丝轻小说", url: "https://www.alcsnovel.com/", desc: "轻小说在线阅读｜日轻翻译", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuYWxjc25vdmVsLmNvbS8=" },
+    { title: "开源阅读", url: "https://gedoor.github.io/", desc: "自定义书源阅读器｜轻小说/网络小说", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9nZWRvb3IuZ2l0aHViLmlvLw==" },
   ]
 });
