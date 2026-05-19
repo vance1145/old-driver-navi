a3d32f793d64af24832c241127989533069e1616
Van <vanjohnpk@gmail.com>
2026-05-20 02:08:42 +0800

    refactor: 🔞 仅保留 Pornhub / XVideos / xHamster 三个主要站点

--- assets/js/data/plus.js
+++ assets/js/data/plus.js
@@ -16,13 +16,6 @@ NAV_DATA.categories.push({
   id: "adult", name: "🔞", icon: "", links: [
     { title: "Pornhub", url: "https://www.pornhub.com/", desc: "全球最大成人平台｜海量免费内容", icon: "" },
     { title: "XVideos", url: "https://www.xvideos.com/", desc: "流量最高的成人视频站｜更新极快", icon: "" },
-    { title: "XNXX", url: "https://www.xnxx.com/", desc: "老牌成人网站｜种类齐全分类细", icon: "" },
-    { title: "RedTube", url: "https://www.redtube.com/", desc: "老牌免费成人站｜界面清爽", icon: "" },
-    { title: "YouPorn", url: "https://www.youporn.com/", desc: "经典成人视频站｜资源丰富", icon: "" },
     { title: "xHamster", url: "https://xhamster.com/", desc: "用户量大｜免费+付费混合模式", icon: "" },
-    { title: "Tube8", url: "https://www.tube8.com/", desc: "历史悠久｜分类细致", icon: "" },
-    { title: "SpankBang", url: "https://spankbang.com/", desc: "新生代成人站｜界面现代", icon: "" },
-    { title: "Eporner", url: "https://www.eporner.com/", desc: "4K 高清内容｜画质最佳", icon: "" },
-    { title: "HQporner", url: "https://hqporner.com/", desc: "高清成人视频合集", icon: "" },
   ]
 });
