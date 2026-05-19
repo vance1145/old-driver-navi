f52fab12167a357f393f7b40afacba5d2696cbea
Van <vanjohnpk@gmail.com>
2026-05-20 03:27:38 +0800

    delete: 移除真白萌/魔笔小说

--- assets/js/data/novel.js
+++ assets/js/data/novel.js
@@ -2,8 +2,6 @@ NAV_DATA.categories.push({
   id: "novel", name: "轻小说", icon: "", links: [
     { title: "轻之国度", url: "https://www.lightnovel.fun/", desc: "国内最大轻小说论坛｜日轻翻译/交流社区", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubGlnaHRub3ZlbC5mdW4v" },
     { title: "哔哩轻小说", url: "https://www.linovelib.com/", desc: "在线阅读/TXT下载｜日轻国轻资源", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubGlub3ZlbGliLmNvbS8=" },
-    { title: "真白萌", url: "https://masiro.me/", desc: "轻小说翻译分享｜Web 文库版", icon: "" },
-    { title: "魔笔小说", url: "https://www.mobinovels.com/", desc: "EPUB/MOBI 下载｜轻小说电子书", icon: "" },
     { title: "成为小说家吧", url: "https://ncode.syosetu.com/", desc: "日本最大 Web 小说站｜转生/异世界", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9uY29kZS5zeW9zZXR1LmNvbS8=" },
     { title: "NovelUpdates", url: "https://www.novelupdates.com/", desc: "英文轻小说追踪｜连载更新查询", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubm92ZWx1cGRhdGVzLmNvbS8=" },
     { title: "BookWalker", url: "https://global.bookwalker.jp/", desc: "角川官方电子书｜日轻正版购买", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9nbG9iYWwuYm9va3dhbGtlci5qcC8=" },
