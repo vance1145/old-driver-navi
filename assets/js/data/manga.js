31fe900f9d2fdb4fa80ab8e82db716803e4a6117
Van <vanjohnpk@gmail.com>
2026-05-20 03:21:53 +0800

    fix: 移除 ComicWalker，修正 MangaBZ 描述

--- assets/js/data/manga.js
+++ assets/js/data/manga.js
@@ -2,7 +2,7 @@ NAV_DATA.categories.push({
   id: "manga", name: "漫画", icon: "", links: [
     { title: "MangaDex", url: "https://mangadex.org/", desc: "全球最大免费漫画站｜多语言/翻译组", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9tYW5nYWRleC5vcmcv" },
     { title: "MangaPlus", url: "https://mangaplus.shueisha.co.jp/", desc: "集英社官方免费｜海贼/咒术/间谍过家家", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9tYW5nYXBsdXMuc2h1ZWlzaGEuY28uanAv" },
-    { title: "MangaBZ", url: "https://mangabz.com/", desc: "英文漫画站｜日漫翻译", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9tYW5nYWJ6LmNvbS8=" },
+    { title: "MangaBZ", url: "https://mangabz.com/", desc: "在线漫画阅读｜日漫/国漫资源", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9tYW5nYWJ6LmNvbS8=" },
     { title: "拷贝漫画", url: "https://www.mangacopy.com/", desc: "在线漫画阅读｜更新快/画质高", icon: "" },
     { title: "快看漫画", url: "https://www.kuaikanmanhua.com/", desc: "国漫社区｜条漫/原创/社交", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cua3VhaWthbm1hbmh1YS5jb20v" },
     { title: "漫客栈", url: "https://www.mkzhan.com/", desc: "国漫在线看｜玄幻/恋爱/搞笑", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cubWt6aGFuLmNvbS8=" },
@@ -12,7 +12,6 @@ NAV_DATA.categories.push({
     { title: "MyComic", url: "https://mycomic.com/cn", desc: "漫画在线阅读｜日漫/国漫", icon: "" },
     { title: "KZO", url: "https://kzo.moe/", desc: "在线漫画阅读｜免费高清", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9rem8ubW9lLw==" },
     { title: "Komiic", url: "https://komiic.com/", desc: "漫画在线阅读｜更新及时", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9rb21paWMuY29tLw==" },
-    { title: "ComicWalker", url: "https://comic-walker.com/", desc: "角川官方免费｜KADOKAWA 作品", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly9jb21pYy13YWxrZXIuY29tLw==" },
     { title: "搜漫", url: "https://www.soman.com/", desc: "漫画搜索引擎｜找漫神器", icon: "https://favicon.png.pub/v1/aHR0cHM6Ly93d3cuc29tYW4uY29tLw==" },
   ]
 });
